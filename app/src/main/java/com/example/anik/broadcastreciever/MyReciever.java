f1280e89ca8d354a8e57941c5c5b9874d93674da
AnikBiswas4 <anikcse3@gmail.com>
2018-10-01 14:07:12 +0600

    app life time  Broadcast Reciever

--- app/src/main/java/com/example/anik/broadcastreciever/MyReciever.java
+++ app/src/main/java/com/example/anik/broadcastreciever/MyReciever.java
@@ -3,10 +3,11 @@ package com.example.anik.broadcastreciever;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MyReciever extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Toast.makeText(context,"action"+intent.getAction(),Toast.LENGTH_LONG).show();
     }
 }
